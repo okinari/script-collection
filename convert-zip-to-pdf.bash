4044247b331e277a902a23cc62178ff276b833aa
okinari <ooba@okinari.com>
2020-08-02 06:54:14 +0900

    zip変換処理なのに、ディレクトリ以外処理されない問題を修正

--- convert-zip-to-pdf.bash
+++ convert-zip-to-pdf.bash
@@ -4,7 +4,5 @@ source ${BASH_SOURCE%/*}/utils/converter-utils.bash
 
 # 引数で指定したzipファイル内の全ての画像ファイルをまとめて1つのpdfファイルに変換する
 for _file_dir in "$@" ; do
-    if [ -d "${_file_dir}" ] ; then
-        convert_zip_to_pdf "${_file_dir}"
-    fi
+    convert_zip_to_pdf "${_file_dir}"
 done
